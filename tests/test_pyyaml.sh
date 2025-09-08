5baf1b957fb51a629bd9c635bdb89e91602b5dad
Guoqing Ge <guoqing.ge@noaa.gov>
2025-09-08 15:35:43 -0600

    rename test scripts to yth, ythx, ytp (#2)

--- tests/test_pyyaml.sh
+++ tests/test_pyyaml.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
+# use the "ytp" script to test the yamltools4jedi based on the PyYAML backend
+# ytp = YamlTools_on_Pyyaml
 
 rm -rf tmp
 mkdir -p tmp
 
-op_pyyaml.py demo.yaml glance  > tmp/level1.txt
-op_pyyaml.py demo.yaml glance "cost function" > tmp/level2.txt
-op_pyyaml.py demo.yaml glance "cost function/observations" > tmp/level3.txt
+ytp demo.yaml glance  > tmp/level1.txt
+ytp demo.yaml glance "cost function" > tmp/level2.txt
+ytp demo.yaml glance "cost function/observations" > tmp/level3.txt
 
 
-op_pyyaml.py demo.yaml traverse "cost function/observations/observers/0/obs filters/0"  > tmp/filter0.txt
+ytp demo.yaml traverse "cost function/observations/observers/0/obs filters/0"  > tmp/filter0.txt
 
 
-op_pyyaml.py demo.yaml dump "test" > tmp/ctest.yaml
+ytp demo.yaml dump "test" > tmp/ctest.yaml
 
-op_pyyaml.py demo.yaml drop "cost function/observations" > tmp/no_obs.yaml
+ytp demo.yaml drop "cost function/observations" > tmp/no_obs.yaml
 
-op_pyyaml.py demo.yaml set_value "output/filename" "ana.nc"  > tmp/ana.yaml
-op_pyyaml.py demo.yaml set_value "variational/iterations/0" geometry.yaml  > tmp/new_geometry.yaml
+ytp demo.yaml set_value "output/filename" "ana.nc"  > tmp/ana.yaml
+ytp demo.yaml set_value "variational/iterations/0" geometry.yaml  > tmp/new_geometry.yaml
 
 
 export YAMLTOOLS_DUMPER="mydumper"
-op_pyyaml.py demo.yaml split
+ytp demo.yaml split
 mv split.demo.yaml tmp/split.mydumper
 
 
 export YAMLTOOLS_DUMPER=""
-op_pyyaml.py demo.yaml split
+ytp demo.yaml split
 mv split.demo.yaml tmp/split.default
 
 
-op_pyyaml.py demo.yaml dump > tmp/org.yaml
-op_pyyaml.py tmp/split.default pack tmp/pack.yaml
+ytp demo.yaml dump > tmp/org.yaml
+ytp tmp/split.default pack tmp/pack.yaml
 diff tmp/org.yaml tmp/pack.yaml
 if (( $? == 0 )); then
   echo "GOOD: split and re-pack generate the identical YAML file"
