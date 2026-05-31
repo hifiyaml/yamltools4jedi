0754a9f9ef17059cf3e0f2a2f8f655e449a916cd
Guoqing Ge <guoqing.ge@noaa.gov>
2026-05-31 00:52:51 -0600

    focus on the hifiyaml backend and update to match the latest hifiyaml 0.1.8 (#4)
    * focus on testing hifiyaml backend
    * update to match the latest hifiyaml 0.1.8

--- tests/test_hifiyaml.sh
+++ tests/test_hifiyaml.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
-# use the "yth" and "ythx" scripts to test the yamltools4jedi based on the hifiyaml backend
-# yth = YamlTools_on_Hifiyaml
-# ythx = yth_eXtended
+# use the "yt" and "ytx" scripts to test the yamltools4jedi based on the hifiyaml backend
+# yt = YamlTools (on hifiyaml)
+# ytx = yt_eXtended
 
 rm -rf tmp
 mkdir -p tmp
 
 export YT_DEDENT=true
-yth demo.yaml dump "cost function/observations/observers/0/obs space" > tmp/ctest1.yaml
+yt demo.yaml dump "cost function/observations/observers/0/obs space" > tmp/ctest1.yaml
 export YT_DEDENT=fase
-yth demo.yaml dump "cost function/observations/observers/0/obs space" > tmp/ctest2.yaml
+yt demo.yaml dump "cost function/observations/observers/0/obs space" > tmp/ctest2.yaml
 
-yth demo.yaml drop "cost function/observations/observers" > tmp/no_obs.yaml
+yt demo.yaml drop "cost function/observations/observers" > tmp/no_obs.yaml
 
-yth demo.yaml modify "output/filename" "filename: ana.nc"  > tmp/ana.yaml
-yth demo.yaml modify "cost function/background error/components/0/covariance" bec_bump.yaml > tmp/becbump.yaml
+yt demo.yaml modify "output/filename" "filename: ana.nc"  > tmp/ana.yaml
+yt demo.yaml modify "cost function/background error/components/0/covariance" bec_bump.yaml > tmp/becbump.yaml
 
 export YT_DEDENT=true
 export YT_SPLIT_LEVEL=1
-yth demo.yaml split
+yt demo.yaml split
 mv split.demo.yaml tmp/split.default_1.0
 
 export YT_DEDENT=fase
 export YT_SPLIT_LEVEL=1
-yth demo.yaml split
+yt demo.yaml split
 mv split.demo.yaml tmp/split.default_1.1
 
 export YT_DEDENT=fase
 export YT_SPLIT_LEVEL=2
-yth demo.yaml split
+yt demo.yaml split
 mv split.demo.yaml tmp/split.default_2
 
 export YT_LIST_INDENT=false
 export YT_PLAIN_PACK=true
-yth tmp/split.default_1.1 pack tmp/pack1.1.yaml
+yt tmp/split.default_1.1 pack tmp/pack1.1.yaml
 diff tmp/pack1.1.yaml demo.yaml
 if (( $? == 0 )); then
   echo "GOOD: split and re-pack generate the identical YAML file"
@@ -43,7 +43,7 @@ fi
 
 export YT_DEDENT=fase
 export YT_PLAIN_PACK=true
-yth tmp/split.default_2 pack  tmp/pack2.yaml
+yt tmp/split.default_2 pack  tmp/pack2.yaml
 diff tmp/pack2.yaml demo.yaml
 if (( $? == 0 )); then
   echo "GOOD: split and re-pack generate the identical YAML file"
@@ -54,16 +54,16 @@ fi
 export YT_USE_CONV_SAT_INFO=false
 export YT_YTYPE="getkf"
 export YT_GETKF_TYPE="solver"
-ythx getkf.yaml > tmp/solver.yaml
+ytx getkf.yaml > tmp/solver.yaml
 
 export YT_USE_CONV_SAT_INFO=false
 export YT_YTYPE="getkf"
 export YT_GETKF_TYPE="post"
-ythx getkf.yaml > tmp/post.yaml
+ytx getkf.yaml > tmp/post.yaml
 
 export YT_USE_CONV_SAT_INFO=true
 export YT_YTYPE="jedivar"
-ythx demo.yaml > tmp/conv_sat_info.yaml
+ytx demo.yaml > tmp/conv_sat_info.yaml
 
 diff -rf tmp ref_hifiyaml 1>/dev/null 2>/dev/null
 if (( $? == 0 )); then
